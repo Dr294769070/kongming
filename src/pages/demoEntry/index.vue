31945913a3fbea02b6932488b2b7714d6d15d8ff
dingjinsong <jinsongding@sohu-inc.com>
2020-12-25 18:05:49 +0800

    演示：移动端1px

---
+++ src/pages/demoEntry/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="appEntry">
+    <div class="item" v-for="(item, index) in demoList" :key="index" @click="toPage(item.path)">
+      <img class="icon" :src="item.icon" />
+      <div class="name">{{ item.name }}</div>
+    </div>
+  </div>
+</template>
+<script>
+import { demoList } from './index.json'
+
+export default {
+  data () {
+    return {
+      demoList
+    }
+  },
+  methods: {
+    toPage(path) {
+      wx.navigateTo({
+        url: path
+      })
+    }
+  },
+  onLoad() {
+  }
+}
+</script>
+
+<style scoped>
+.appEntry{
+  box-sizing: border-box;
+  padding: 20rpx;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  justify-content: space-between;
+}
+.appEntry .item{
+  width: 150rpx;
+  padding: 20rpx;
+  display: inline-flex;
+  flex-direction: column;
+  align-items: center;
+  border: 2rpx solid#E4E7ED;
+  border-radius: 10rpx;
+  margin: 10rpx;
+  box-shadow: 0 2rpx 4rpx rgba(0, 0, 0, .12), 0 0 6rpx rgba(0, 0, 0, .04)
+}
+.item .icon {
+  width: 96rpx;
+  height: 96rpx;
+  margin-bottom: 20rpx;
+}
+.item .name{
+  height: 30rpx;
+  line-height: 30rpx;
+  text-align: center;
+  font-size: 24rpx;
+}
+</style>
---
+++ src/pages/demos/onePx/index.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="onePx">
+        <div class="item"></div>
+        <!-- 
+            设计稿给的是物理像素 1px
+            css写的是逻辑像素 1px
+            如果设备的分辨率很高，比如视网膜屏幕 1px用物理两个像素点显示（DRP）
+            那么就需要对其进行缩放 使用伪元素和scale
+            这里实现了四个边框，如果只有一个边要显示，就用border-top等即可
+            考虑到不同设备的DPR不一样，还可以用css3的@media多媒体查询功能，针对不同屏幕应用不同样式
+         -->
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {};
+    },
+};
+</script>
+<style lang="scss" scoped>
+@import '../../../styles/common.scss';
+
+.onePx {
+    height: 100vh;
+    .item {
+        margin: 20rpx;
+        width: 100rpx;
+        height: 100rpx;
+        @include m-r1b(#d5d5d5, 10rpx);
+    }
+    // .onePxBorder {
+    //     position: relative;
+    //     &::after {
+    //         content: "";
+    //         position: absolute;
+    //         left: 0;
+    //         top: 0;
+    //         width: 200%;
+    //         height: 200%;
+    //         box-sizing: border-box;
+    //         border: 1px solid #d5d5d5;
+    //         border-radius: 10rpx;
+    //         transform: scale(0.5); // 2d方向x、y等比缩小为0.5倍
+    //     }
+    // }
+}
+</style>
